5c2c734e888554536b9d4e523e431448cd7d5ed5
HelloHello9231 <hs9323152@gmail.com>
2022-02-11 22:30:02 +0000

    data

--- weather.js
+++ weather.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch")
 
-api_key = "5a1900c39d7e4c3fb71192249221102"
-coutry = "London"
-current_weather = `http://api.weatherapi.com/v1/current.json?key={api_key}&q={country}`
+let api_key = "5a1900c39d7e4c3fb71192249221102"
+
 
 router.get("/country",async (req,res) => 
 {
-    print(req)
+    try {
+        let country = req.query.c
+        let current_weather = `http://api.weatherapi.com/v1/current.json?key={api_key}&q={country}`
+        let data = await fetch(current_weather)
+        console.log(data)
+        return data 
+    }
+    catch(e){
+        console.log("could not get data")
+        console.log(e)
+    }
 })
+
+module.exports = router;
